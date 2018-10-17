12d50ab8dfdf6944804aba649312f07f1d81907f
Julian Szczurowski <julian.szczurowski@gmail.com>
2018-10-17 11:55:36 +0200

    Added a logging interface

---
+++ src/Insta.Common/ILogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Insta.Common
+{
+    public interface ILogger
+    {
+        void Error(Exception exception);
+    }
+}
